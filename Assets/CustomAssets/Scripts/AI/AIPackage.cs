99eab307dff74cf5906425c3c5b67929b4688316
Andrew Giles <agiles231@gmail.com>
2017-09-28 18:26:51 -0400

    -Add many path finding agents to NetworkMain scene as a stress test. -Add many path finding obstacles to NetworkMain scene as a stress test. -Add Editor window to assist in duplicating objects to be used to randomly create obstacles and agents. -Add test script for obstacles to simulate movement

--- Assets/CustomAssets/Scripts/AI/AIPackage.cs
+++ Assets/CustomAssets/Scripts/AI/AIPackage.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu()]
 public class AIPackage : ScriptableObject {
     public Vector3 getTarget () {
-        return GameObject.Find("PrancingPony").transform.position;
+        //return GameObject.Find("marker_PrancingPony").transform.position;
+        return new Vector3(Random.Range(10.0f, 450.0f), Random.Range(1.0f, 2.0f), Random.Range(10.0f, 450.0f));
     }
 }
---
+++ Assets/CustomAssets/Scripts/Test/TestObstacleMovementFPS.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestObstacleMovementFPS : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.position += new Vector3(Random.Range(-0.1f, 0.1f), 0.0f, Random.Range(-0.1f, 0.1f));
+        //transform.position += new Vector3(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+    }
+}
