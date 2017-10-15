b101c4de8fdb6d0972c57c7fa1af0eed2f83778f
Andrew Giles <agiles231@gmail.com>
2017-10-15 18:33:31 -0400

    -Add Interact interface.

---
+++ Assets/CustomAssets/Scripts/Interfaces/Interactable.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class InteractableMonobehaviour : MonoBehaviour {
+
+    // interact function
+    public virtual void Interact() { }
+}
